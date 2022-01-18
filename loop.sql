dc3c031ba556b48a5debc7c9eefdb18de6650dda
Nikolay-spec <55550441+Nikolay-spec@users.noreply.github.com>
2022-01-18 15:03:21 +0200

    Update loop.sql

--- loop.sql
+++ loop.sql
@@ -6,19 +6,17 @@ select * from clubscopy;
 DO $$
 DECLARE
     team_id     clubscopy.team_id%TYPE;
-    team_logo   clubscopy.team_logo%TYPE;
-	  team_name   clubscopy.team_name%TYPE;
+	team_name   clubscopy.team_name%TYPE;
 	
 
 BEGIN
     team_id := 10000;
-	  team_logo := 'Script';
     team_name := 'Name';
 	
     FOR counter IN 1..10
         LOOP
-            INSERT INTO clubscopy(team_id, team_logo, team_name)
-            VALUES (team_id + counter, team_logo || counter, team_name || counter);
+            INSERT INTO clubscopy(team_id, team_name)
+            VALUES (team_id + counter, team_name || counter);
         END LOOP;
 END;
 $$
